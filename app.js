cbb808a88d1ea72511d88139254c2d7e9e31ac87
meeshlin <meesh.lin@icloud.com>
2016-05-10 19:31:11 -0700

    Added code

--- app.js
+++ app.js
@@ -4,18 +4,23 @@ $(document).ready(function() {
     $("#score").html(score);
 
   $(document).on("click", "#increase-5", function () {
+    //Step 1: Add 5 to score variable
+    //Step 2: Represent the new score on the UI
     score=score+5;
-    $("#score").html(score);
+    $("#score").html(score + " Points");
   });
 
   $(document).on("click", "#decrease-5", function () {
-    score=score-5;
-    $("#score").html(score);
+    if (score-5>=0) {
+      score=score-5;
+      // Score +- 5;
+    }
+    $("#score").html(score + " Points");
   });
 
   $(document).on("click", "#submit-custom-score", function () {
     score=parseInt($("#custom-score").val());
-    $("#score").html(score);
+    $("#score").html(score + " Points");
   });
 
 });
